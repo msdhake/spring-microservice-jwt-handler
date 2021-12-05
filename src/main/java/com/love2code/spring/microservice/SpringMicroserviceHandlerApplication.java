925a1f55bf38dfeede8399cd89f1973ad64d3afa
msdhake <manojdhake62@gmail.com>
2021-12-05 12:48:05 +0530

    This is spring server to run the JWT Spring project

---
+++ src/main/java/com/love2code/spring/microservice/SpringMicroserviceHandlerApplication.java
@@ -0,0 +1,15 @@
+package com.love2code.spring.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringMicroserviceHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceHandlerApplication.class, args);
+	}
+
+}
